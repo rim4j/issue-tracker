bcafd98d1b84447411574644d7f6a5fccbeede6a
rim4j <amirhosseinjalali818@gmail.com>
2024-08-07 18:08:43 +0330

    build new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,11 @@
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 const IssuesPage = () => {
   return (
     <div>
-      <Button>new issue</Button>
+      <Button>
+        <Link href='/issues/new'>New Issues</Link>
+      </Button>
     </div>
   );
 };
