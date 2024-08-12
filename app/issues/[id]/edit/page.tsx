4b062cdc9f3abc4f8b634ed702f3bda0d8634757
rim4j <amirhosseinjalali818@gmail.com>
2024-08-12 10:35:01 +0330

    improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import IssueForm from '../../_components/IssueForm';
 import { prisma } from '@/prisma/client';
 import { notFound } from 'next/navigation';
+import IssueFormSkeleton from './loading';
+import dynamic from 'next/dynamic';
 
+const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 interface Props {
   params: { id: string };
 }
