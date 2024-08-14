1177f1fa430c78b9625c14cbb7ab8fe8424098f0
rim4j <amirhosseinjalali818@gmail.com>
2024-08-14 11:42:07 +0330

    change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { FaBug } from 'react-icons/fa6';
 import classnames from 'classnames';
 import { usePathname } from 'next/navigation';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -18,35 +18,41 @@ const NavBar = () => {
     { label: 'Issues', href: '/issues/list' },
   ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href='/'>
-        <FaBug />
-      </Link>
-      <ul className='flex space-x-6'>
-        {links.map((item, i) => (
-          <li key={i}>
-            <Link
-              className={classnames({
-                'text-zinc-900': item.href === currentPath,
-                'text-zinc-500': item.href !== currentPath,
-                'hover:text-zinc-800 transition-colors': true,
-              })}
-              // className='text-zinc-500 hover:text-zinc-800 transition-colors'
-              href={item.href}
-            >
-              {item.label}
+    <nav className=' border-b mb-5 px-5 h-14 py-3  '>
+      <Container>
+        <Flex justify='between'>
+          <Flex align='center' gap='3'>
+            <Link href='/'>
+              <FaBug />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === 'authenticated' && (
-          <Link href='api/auth/signout'>Log out</Link>
-        )}
-        {status === 'unauthenticated' && (
-          <Link href='api/auth/signin'>Login</Link>
-        )}
-      </Box>
+            <ul className='flex space-x-6'>
+              {links.map((item, i) => (
+                <li key={i}>
+                  <Link
+                    className={classnames({
+                      'text-zinc-900': item.href === currentPath,
+                      'text-zinc-500': item.href !== currentPath,
+                      'hover:text-zinc-800 transition-colors': true,
+                    })}
+                    // className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                    href={item.href}
+                  >
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'authenticated' && (
+              <Link href='api/auth/signout'>Log out</Link>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href='api/auth/signin'>Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
